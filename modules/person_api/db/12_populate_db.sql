befa827e1134467b8cc158e08a78dfd6108480f0
dbritto-dev <dbritto.dev@gmail.com>
2021-10-15 14:46:19 -0500

    Update person api

--- modules/person_api/db/12_populate_db.sql
+++ modules/person_api/db/12_populate_db.sql
@@ -1,25 +1,5 @@
-#!/bin/sh
-
-set -e
-
-# Perform all actions as $POSTGRES_USER
-export PGUSER="$POSTGRES_USER"
-
-echo "Creating table"
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<- 'EOSQL'
-CREATE TABLE persons (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    company_name VARCHAR NOT NULL
-);
-EOSQL
-
-echo "Populating table"
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<- 'EOSQL'
 INSERT INTO persons (id, first_name, last_name, company_name) VALUES (5, 'Taco', 'Fargo', 'Alpha Omega Upholstery');
 INSERT INTO persons (id, first_name, last_name, company_name) VALUES (6, 'Frank', 'Shader', 'USDA');
 INSERT INTO persons (id, first_name, last_name, company_name) VALUES (1, 'Pam', 'Trexler', 'Hampton, Hampton and McQuill');
 INSERT INTO persons (id, first_name, last_name, company_name) VALUES (8, 'Paul', 'Badman', 'Paul Badman & Associates');
 INSERT INTO persons (id, first_name, last_name, company_name) VALUES (9, 'Otto', 'Spring', 'The Chicken Sisters Restaurant');
-EOSQL
--- modules/person_api/run.py
+++ modules/person_api/run.py
@@ -2,16 +2,16 @@
 import logging
 
 # Third-party packages
-import dotenv
-import waitress
+from dotenv import load_dotenv
+from waitress import serve
 
 # Local packages
 from person_app import create_app
 
-dotenv.load_dotenv()
+load_dotenv()
 
 app = create_app()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    waitress.serve(app)
+    serve(app)
