2dbea059474da4706dab56840472596da5031917
dbritto-dev <dbritto.dev@gmail.com>
2021-10-17 09:23:56 -0500

    Update person api

--- modules/person_api/person_database.py
+++ modules/person_api/person_database.py
@@ -9,9 +9,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+DB_USER = os.getenv("DB_USERNAME")
+DB_PASS = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+
 
 def register_db(app: Flask) -> None:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
