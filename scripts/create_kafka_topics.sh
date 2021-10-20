fa8ca71c00fdd1788d84ee3f70fde374dc539a0d
dbritto-dev <dbritto.dev@gmail.com>
2021-10-20 02:01:49 -0500

    Update script

--- scripts/create_kafka_topics.sh
+++ scripts/create_kafka_topics.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl exec -i $1 $2 -- bash -c "kafka-topics --create --bootstrap-server $BOOTSTRAP_SERVER --replication-factor 1 --partitions 1 --topic Persons"
-kubectl exec -i $1 $2 -- bash -c "kafka-topics --create --bootstrap-server $BOOTSTRAP_SERVER --replication-factor 1 --partitions 1 --topic Locations"
+kubectl exec -i $1 --container broker-server -- bash -c "kafka-topics --create --bootstrap-server localhost:29092 --replication-factor 1 --partitions 1 --topic Persons"
+kubectl exec -i $1 --container broker-server -- bash -c "kafka-topics --create --bootstrap-server localhost:29092 --replication-factor 1 --partitions 1 --topic Locations"
